1c77be4b658f0d7c98c204da60107432f2935c37
Luis_2006 <rodriguezmosqueraluisleonardo@gmail.com>
2026-03-27 00:35:09 -0500

    Iniciando El Scanner

--- Ciclos.java
+++ Ciclos.java
@@ -3,3 +3,7 @@ import java.util.Scanner;
 public class Ciclos {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+
+        while (opcion !=1) {
