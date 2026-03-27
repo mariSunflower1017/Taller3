3fbfcee747a741d9e762784e5361eb9eb01a5c82
Luis_2006 <rodriguezmosqueraluisleonardo@gmail.com>
2026-03-27 00:36:03 -0500

    Ingreso Del Menú ASCII Art

--- Ciclos.java
+++ Ciclos.java
@@ -7,3 +7,38 @@ public class Ciclos {
         int opcion = 0;
 
         while (opcion !=1) {
+            
+System.out.println("                     ,---.           ,---.");
+System.out.println("                    / /\"`.\\.--\"\"\"--./,'\"\\ \\");
+System.out.println("                    \\ \\    _       _    / /");
+System.out.println("                     `./  / __   __ \\  \\,'");
+System.out.println("                      /    /_O)_(_O\\    \\");
+System.out.println("                      |  .-'  ___  `-.  |");
+System.out.println("                   .--|       \\_/       |--.");
+System.out.println("                 ,'    \\   \\   |   /   /    `.");
+System.out.println("                /       `.  `--^--'  ,'       \\");
+System.out.println("             .-\"\"\"\"\"-.    `--.___.--'     .-\"\"\"\"\"-.");
+System.out.println(".-----------/         \\------------------/         \\--------------.");
+System.out.println("| .---------\\         /----------------- \\         /------------. |");
+System.out.println("| |          `-`--`--'                    `--'--'-'             | |");
+System.out.println("| |                                                             | |");
+System.out.println("| |                                                             | |");
+System.out.println("| |                                                             | |");
+System.out.println("| |                1. Salir                                     | |");
+System.out.println("| |                2. Suma N-avo numero de fibonacci            | |");
+System.out.println("| |                3. N-avo numero primo                        | |");
+System.out.println("| |                4. Raíces ecuacion cuadratica                | |");
+System.out.println("| |                5. Promedio de N numeros                     | |");
+System.out.println("| |                                                             | |");
+System.out.println("| |                                                             | |");
+System.out.println("| |                                                             | |");
+System.out.println("| |                                                             | |");
+System.out.println("| |_____________________________________________________________| |");
+System.out.println("|_________________________________________________________________|");
+System.out.println("                   )__________|__|__________(");
+System.out.println("                  |            ||            |");
+System.out.println("                  |____________||____________|");
+System.out.println("                    ),-----.(      ),-----.(");
+System.out.println("                  ,'   ==.   \\    /  .==    `.");
+System.out.println("                 /            )  (            \\");
+System.out.println("                 `==========='    `===========' ");
